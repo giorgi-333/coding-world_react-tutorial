43cce7bba1827f103d4ad3669b94af260335f0ab
giorgi-333 <Sachxereze123>
2023-02-10 10:17:24 +0400

    Added counter lesson #01

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import surati from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [ricxvi,ricxvisMinicheba] = useState(3)
+  const kleba = ()=>{
+    if(ricxvi > 0) {
+      ricxvisMinicheba(ricxvi-1)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='counter-cont'>
+        <button onClick={()=>{ ricxvisMinicheba(ricxvi+1) }}>გაზრდა</button>
+        <div className='count'> {ricxvi} </div>
+        <button onClick={kleba}>კლება</button>
     </div>
   );
 }
